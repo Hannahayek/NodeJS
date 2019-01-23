13ccad68519e996cff57888290126f94568b1ca2
Hanna Hayek (Pactera) <v-hahaye@microsoft.com>
2019-01-23 15:22:05 +0800

    29. Sending Responses

--- section-3/app.js
+++ section-3/app.js
@@ -1,6 +1,9 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    console.log(req);
+    console.log(req.url,req.method,req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html><head><title>my first page</title><body>hello from my node.js server</body></head></html>');
+    res.end();
 });
 
 server.listen(3000);
