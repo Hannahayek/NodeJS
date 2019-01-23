1aaed021d36586b19224ef5c2208d880c70d9d3a
hayek <hayek@HANNA-PC>
2019-01-23 21:48:38 +0800

    32. Redirecting Requests

--- section-3/app.js
+++ section-3/app.js
@@ -1,12 +1,19 @@
 const http=require('http');
+const fs=require('fs');
 const server=http.createServer((req,res)=>{
 const url=req.url;
+const method=req.method;
 if(url==='/'){
     res.setHeader('Content-Type','text/html');
     res.write('<html><head><title>Enter message</title><body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body></head></html>');
    return res.end(); //this will quit the function execution 
 }
-
+    if(url==='/message' && method==='POST'){
+     fs.writeFileSync('message.txt','Dummay ');
+     res.statusCode=302;
+     res.setHeader('Location','/');
+      return res.end(); 
+    }
     console.log(req.url,req.method,req.headers);
     res.setHeader('Content-Type','text/html');
     res.write('<html><head><title>my first page</title><body>hello from my node.js server</body></head></html>');
