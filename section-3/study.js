32dceb9025c5c9c2ec34946ba58a3499e1c70228
Hanna Hayek (Pactera) <v-hahaye@microsoft.com>
2019-01-27 17:37:18 +0800

    studies

---
+++ section-3/study.js
@@ -0,0 +1,58 @@
+/* const nums = [1,1,2];
+remove duplicates
+var threeSum = function(nums) {
+    var sortedArray = nums.sort();
+    var len = sortedArray.length - 1;
+    var newArr = [];
+
+    if (len >= 0) {
+        for (var i = 0; i < len; i++) {
+            if (sortedArray[i] !== sortedArray[i + 1]) {
+                newArr.push(sortedArray[i]);
+            }
+        }
+        newArr.push(sortedArray[len]);
+    }
+    return newArr
+
+
+
+};
+
+var output=threeSum(nums);
+console.log(output); */
+
+/* //reverse string
+var input=["h","e","l","l","o"];
+var reverseString = function(s) {
+    var reversed=s.reverse();
+    return reversed
+};
+
+
+console.log(reverseString(input)); *
+
+/* //reverse integers https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/880/ */
+
+/* var reverse = function(num) {
+var reverse=num.toString();
+let output=reverse.split('').reverse().join('');
+let final=parseInt(output)*Math.sign(num);
+
+if(Math.abs(final)>Math.pow(2,31)){
+    final=0;
+}
+
+
+return final;
+};
+
+console.log(reverse(-1235));
+ */
+
+
+ //find first unique caharcters
+
+ var firstUniqChar = function(s) {
+    
+};
