8f85fc810a315816996e00de46c673058d1c0ad3
hayek <hayek@HANNA-PC>
2019-03-11 21:50:19 +0800

    section 3

--- section-3/routes.js
+++ section-3/routes.js
@@ -1,10 +1,9 @@
-const http = require('http');
-const fs = require('fs');
+const fs=require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
+const requestHandler=(req,res)=>{
+    const url=req.url;
+    const method=req.method;
+if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
     res.write(
@@ -35,6 +34,7 @@ const server = http.createServer((req, res) => {
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
   res.end();
-});
 
-server.listen(3000);
+};
+
+module.exports=requestHandler;   //we expose the function global
