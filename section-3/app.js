39ae46eea02b3b0501f6dca10d72f1688333c4a0
hayek <hayek@HANNA-PC>
2019-03-11 22:05:30 +0800

    section 4

--- section-3/app.js
+++ section-3/app.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 
 const routes=require('./routes');
 
-const server = http.createServer(routes);  //will exexute function for incoming requests
+const server = http.createServer(routes);  //will exexute function for incoming requestsn
 
 server.listen(3000);
