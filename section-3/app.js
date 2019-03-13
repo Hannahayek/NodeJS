46069b1bf8d275032d24c451383a2e2355a5073e
Hanna Hayek (Pactera) <v-hahaye@microsoft.com>
2019-03-13 18:27:03 +0800

    64. Limiting Middleware Execution to POST Requests

--- section-3/app.js
+++ section-3/app.js
@@ -15,8 +15,8 @@ app.use('/add-product',(req,res,next)=>{
     
    res.send(`<form action="/product" method="POST"><input type="text" name ="title"><button type="submit">Add Product</button></form>`);
     }); 
-
-app.use('/product',(req,res,next)=>{
+//we change to post to trigger only with post request
+app.post('/product',(req,res,next)=>{
 console.log(req.body);
 res.redirect('/');
 });
