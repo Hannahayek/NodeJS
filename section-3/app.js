677dc4480a78a4dcdac1a2723b692cc50dd7baf3
Hanna Hayek (Pactera) <v-hahaye@microsoft.com>
2019-03-13 18:47:24 +0800

    65. Using Express Router

--- section-3/app.js
+++ section-3/app.js
@@ -1,32 +1,18 @@
 const http = require('http');
-const express=require('express')
+const express=require('express');
+
+const adminRoutes=require('./routes/admin')
+const shopRoutes=require('./routes/shop');
 const bodyParser=require('body-parser'); //npm install --save body-parser
 const app=express();
 
-/* app.use('/',(req,res,next)=>{
-  
-    console.log("this always runs")
-    next(); //allows the request to continue to next middle ware
-    });  */
 
-  app.use(bodyParser.urlencoded({extended:false}));// will do body parse for us
+app.use(bodyParser.urlencoded({extended:false}));// will do body parse for us
 
-app.use('/add-product',(req,res,next)=>{
-    
-   res.send(`<form action="/product" method="POST"><input type="text" name ="title"><button type="submit">Add Product</button></form>`);
-    }); 
-//we change to post to trigger only with post request
-app.post('/product',(req,res,next)=>{
-console.log(req.body);
-res.redirect('/');
-});
+//order matters  the / should be last
+app.use(adminRoutes)
+app.use(shopRoutes);
 
-
-//to add middleware function will executed with every incoming request
-app.use('/',(req,res,next)=>{
-
-res.send("<h1>hello from express</h1>");
-}); 
 
 
 
