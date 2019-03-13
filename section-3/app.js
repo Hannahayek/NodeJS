f4fb3299ef22df8cb6906110aa551e8ca507eb7f
Hanna Hayek (Pactera) <v-hahaye@microsoft.com>
2019-03-13 16:00:27 +0800

    63. Parsing Incoming Requests

--- section-3/app.js
+++ section-3/app.js
@@ -1,22 +1,30 @@
 const http = require('http');
 const express=require('express')
-
+const bodyParser=require('body-parser'); //npm install --save body-parser
 const app=express();
 
-app.use('/',(req,res,next)=>{
+/* app.use('/',(req,res,next)=>{
   
     console.log("this always runs")
     next(); //allows the request to continue to next middle ware
-    }); 
+    });  */
+
+  app.use(bodyParser.urlencoded({extended:false}));// will do body parse for us
 
 app.use('/add-product',(req,res,next)=>{
-    console.log("in the middleware!");
-  //  next(); //allows the request to continue to next middle ware
-     res.send("<h1>add product page</h1>");
+    
+   res.send(`<form action="/product" method="POST"><input type="text" name ="title"><button type="submit">Add Product</button></form>`);
     }); 
+
+app.use('/product',(req,res,next)=>{
+console.log(req.body);
+res.redirect('/');
+});
+
+
 //to add middleware function will executed with every incoming request
 app.use('/',(req,res,next)=>{
-console.log("in the middleware!");
+
 res.send("<h1>hello from express</h1>");
 }); 
 
