5075e65140f36099cdf95e2ac0a36f0cd6d8607f
hayek <hayek@HANNA-PC>
2019-03-15 00:15:22 +0800

    67. Filtering Paths

--- section-3/app.js
+++ section-3/app.js
@@ -10,14 +10,13 @@ const app=express();
 app.use(bodyParser.urlencoded({extended:false}));// will do body parse for us
 
 //order matters  the / should be last
-app.use(adminRoutes)
+//any start with admin take admin routes
+app.use('/admin',adminRoutes)
 app.use(shopRoutes);
 
-
 //set error page 
-
 app.use((req,res,next)=>{
-res.status(404).send("<h1>Page Note Found</h1>")
+res.status(404).send("<h1>Page Not  Found</h1>")
 });
 
 
--- section-3/routes/admin.js
+++ section-3/routes/admin.js
@@ -1,13 +1,15 @@
 const express=require('express');
 
 const router=express.Router();
-
+// /admin/add-product =>Get
 router.get('/add-product',(req,res,next)=>{
     
     res.send(`<form action="/product" method="POST"><input type="text" name ="title"><button type="submit">Add Product</button></form>`);
      }); 
+
+  // /admin/add-product =>post   
  //we change to post to trigger only with post request
- router.post('/product',(req,res,next)=>{
+ router.post('/add-product',(req,res,next)=>{
  console.log(req.body);
  res.redirect('/');
  });
