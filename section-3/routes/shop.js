24178b413b25f6fe49b2d998374690e9d585cb85
hayek <hayek@HANNA-PC>
2019-03-16 21:28:35 +0800

    80. Outputting Dynamic Content

--- section-3/routes/shop.js
+++ section-3/routes/shop.js
@@ -9,8 +9,8 @@ const rootDir=require('../helpers/path');
 const adminRoutes=require('./admin')
 //path will auto  build the path for us
 router.get('/',(req,res,next)=>{
-   console.log(adminRoutes.products);
- res.sendFile(path.join(rootDir,'views','shop.html'));
+   const products=adminRoutes.products;
+  res.render('shop',{prods:products,docTitle:'Shop'});
   
     }); 
 
