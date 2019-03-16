b32b83e8d2ee90edb6ba6086f31766a9344d5d0f
hayek <hayek@HANNA-PC>
2019-03-16 23:30:33 +0800

    86. Converting our Project to Handlebars

--- section-3/routes/shop.js
+++ section-3/routes/shop.js
@@ -10,7 +10,7 @@ const adminRoutes=require('./admin')
 //path will auto  build the path for us
 router.get('/',(req,res,next)=>{
    const products=adminRoutes.products;
-  res.render('shop',{prods:products,docTitle:'Shop',path:'/'});
+  res.render('shop',{prods:products,docTitle:'Shop',path:'/',hasProducts:products.length>0});
   
     }); 
 
