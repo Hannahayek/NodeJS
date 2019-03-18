9e0f0d4c2bc8e487498609b8002451d0224f5ebd
hayek <hayek@HANNA-PC>
2019-03-18 21:06:27 +0800

    assigment 4

---
+++ assigment4/app.js
@@ -0,0 +1,29 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const app=express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const users=[];
+
+app.get('/',(req,res,next)=>{
+res.render('index',{PageTitle:"Add User"});
+});
+
+app.get('/users',(req,res,next)=>{
+res.render('users',{PageTitle:'User',users:users});
+});
+
+
+app.post('/add-user',(req,res,next)=>{
+     users.push({name: req.body.username});
+     res.redirect('/users')
+});
+
+
+
+
+app.listen(3000)
