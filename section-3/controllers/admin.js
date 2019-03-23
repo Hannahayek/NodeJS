72087297e38aaf99a3d54ae8480e31a178059291
hayek <hayek@HANNA-PC>
2019-03-23 21:47:54 +0800

    122. Linking to the Edit Page

--- section-3/controllers/admin.js
+++ section-3/controllers/admin.js
@@ -4,7 +4,8 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
    res.render('admin/edit-product', {
       pageTitle: 'Add Product',
-      path: '/admin/add-product'
+      path: '/admin/add-product',
+      editing:false
        });
   };
   
@@ -20,16 +21,28 @@ exports.getAddProduct = (req, res, next) => {
   };
 
   exports.getEditProduct = (req, res, next) => {
-    const editMode=req.query.edit;
-    if(!editMode){
+    const editMode = req.query.edit;
+    if (!editMode) {
       return res.redirect('/');
     }
-    res.render('admin/edit-product', {
-       pageTitle: 'Edit Product',
-       path: '/admin/edit-product',
-       editing:editMode
-        });
-   };
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+    });
+  };
+
+  exports.postEditProduct=(req,res,next) =>{
+      
+  };
+
   exports.getProducts= (req, res, next) => {
   Product.fetchAll(products => {
     res.render('admin/products', {
--- section-3/routes/shop.js
+++ section-3/routes/shop.js
@@ -8,16 +8,16 @@ const router = express.Router();
 
 router.get('/', shopController.getIndex);
 
-router.get('/products',shopController.getProducts);
-           //should be prfeixed by : so we can extract it in controller 
-router.get('/products/:productId',shopController.getProduct);
+router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart',shopController.getCart);
-router.post('/cart',shopController.postCart);
+router.get('/cart', shopController.getCart);
 
-router.get('/checkout',shopController.getCheckout);
+router.post('/cart', shopController.postCart);
 
-router.get('/orders',shopController.getOrders);
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
