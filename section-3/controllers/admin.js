99508c7b2c5d061be9e5f9f0fdf3bf8943152802
hayek <hayek@HANNA-PC>
2019-03-25 22:37:39 +0800

    156. Deleting Products

--- section-3/controllers/admin.js
+++ section-3/controllers/admin.js
@@ -20,6 +20,7 @@ exports.postAddProduct = (req, res, next) => {
     description:description
   }).then(result =>{
     console.log("Created Product");
+    res.redirect('/admin/products');
   }).catch(err =>{
     console.log(err);
   });
@@ -86,6 +87,14 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+ Product.findByPk(prodId)
+ .then(product=>{
+   return product.destroy();
+ })
+ .then(result =>{
+   console.log("product deleted")
+   res.redirect('/admin/products');
+ })
+ .catch(err=>console.log(err));
+ 
 };
