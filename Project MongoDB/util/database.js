0afa8e1e366ddc7a49fd04efefc9e66250dc112d
Hanna Hayek (Pactera) <v-hahaye@microsoft.com>
2019-03-28 15:47:04 +0800

    178. Finishing the Database Connection

--- Project MongoDB/util/database.js
+++ Project MongoDB/util/database.js
@@ -2,14 +2,26 @@ const mongodb=require('mongodb');
 
 const MongoClient=mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect=callback =>{
-   MongoClient.connect('mongodb+srv://hannahayek:GTfcVZ7hTEJS3mY@cluster0-gdrrc.mongodb.net/test?retryWrites=true')
+   MongoClient.connect('mongodb+srv://hannahayek:GTfcVZ7hTEJS3mY@cluster0-gdrrc.mongodb.net/shop?retryWrites=true')
    .then(client =>{
       console.log("Connected");
+      _db=client.db();  //store connection
       callback(client);
    }).catch(err=>{
       console.log(err);
-   })
-}
+      throw err;
+   });
+};
 
-module.exports=mongoConnect;
+
+const getDb=()=>{
+   if(_db){
+      return _db
+   }
+   throw 'No Database found'
+};
+exports.mongoConnect=mongoConnect;
+exports.getDb=getDb;
