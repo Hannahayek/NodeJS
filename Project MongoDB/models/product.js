a9b7409dac2aba3b77e74014ae0dd6ba13638c07
hayek <hayek@HANNA-PC>
2019-04-01 01:57:49 +0800

    187. One Note About Updating Products

--- Project MongoDB/models/product.js
+++ Project MongoDB/models/product.js
@@ -2,18 +2,27 @@ const getDb = require('../util/database').getDb;
 const mongodb=require('mongodb');
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl,id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id=new mongodb.ObjectId(id);
   }
 
   save() {
     const db = getDb();
-    return db
+    let dbOp;
+    if(this._id){
+      //update the product
+      dbOp=db.collection('products').updateOne({_id: this._id},{$set:this});  //this will update all fields but not ID    
+    }else{
+      dbOp=db
       .collection('products')
       .insertOne(this)
+    }
+    return dbOp
+     
       .then(result => {
         console.log(result);
       })
