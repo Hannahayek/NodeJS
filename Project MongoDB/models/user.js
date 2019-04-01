ac8dd98bb8325c98ba7065ecfa99748faef0e947
hayek <hayek@HANNA-PC>
2019-04-01 22:58:09 +0800

    190. Creating New Users

--- Project MongoDB/models/user.js
+++ Project MongoDB/models/user.js
@@ -1,16 +1,44 @@
-const Sequelize = require('sequelize');
+const getDb = require('../util/database').getDb;
+const mongodb=require('mongodb');
 
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+class User{
+  constructor(username,email,id){
+  this.name=username;
+  this.email=email;
+  this._id= id ? new mongodb.ObjectId(id):null;
+  }
+  save(){
+    
+    const db = getDb();
+    let dbOp;
+    if(this._id){
+      //update the product
+      dbOp=db.collection('users').updateOne({_id: this._id},{$set:this});  //this will update all fields but not ID    
+    }else{
+      dbOp=db
+      .collection('users')
+      .insertOne(this)
+    }
+    return dbOp
+     
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  static findById(userId){
+    const db=getDb();                                         //_id saved as object
+     return db.collection('users').find({_id:new mongodb.ObjectId(userId)})
+     .next() //to get last id returned
+     .then(user =>{
+      return user;
+     })
+     .catch(err =>{
+       console.log(err);
+     })
+  }
+}
 
 module.exports = User;
