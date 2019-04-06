2b1bb19c6f800a89dbf8558cb79bee89b57681cf
hayek <hayek@HANNA-PC>
2019-04-06 22:44:12 +0800

    223. Getting & Displaying the Orders

--- Project Mongoose/models/user.js
+++ Project Mongoose/models/user.js
@@ -73,7 +73,7 @@ userSchema.methods.addOrder=function(){
 }
 
 
-userSchema.methods.clarCart=function(){
+userSchema.methods.clearCart=function(){
  this.cart={items:[]};
  return this.save();
 }
@@ -84,29 +84,7 @@ module.exports=mongoose.model('User',userSchema);
 
 
 
-//   addOrder() {
-//     const db = getDb();
-//     return this.getCart()
-//       .then(products => {
-//         const order = {
-//           items: products,
-//           user: {
-//             _id: new ObjectId(this._id),
-//             name: this.name
-//           }
-//         };
-//         return db.collection('orders').insertOne(order);
-//       })
-//       .then(result => {
-//         this.cart = { items: [] };
-//         return db
-//           .collection('users')
-//           .updateOne(
-//             { _id: new ObjectId(this._id) },
-//             { $set: { cart: { items: [] } } }
-//           );
-//       });
-//   }
+
 
 //   getOrders() {
 //     const db = getDb();
