c5a98d2cf5178a78fd2c55b7d212b1d50868a721
hayek <hayek@HANNA-PC>
2019-04-19 16:56:01 +0800

    277. Adding Logic to Update the Password

--- Project Mongoose/controllers/auth.js
+++ Project Mongoose/controllers/auth.js
@@ -180,21 +180,43 @@ exports.getNewPassword=(req,res,next) => {
   const token=req.params.token;
   User.findOne({resetToken:token,resetTokenExpiration:{$gt:Date.now()}})
   .then(user =>{
-
-  })
-  .catch(err =>console.log(err))
-  
-  let message=req.flash('error');
-  if(message.length>0){
-    message=message[0] 
- }else{
-   message=null;
- }
-  res.render('auth/new-password', {
+    let message=req.flash('error');
+    if(message.length>0){
+      message=message[0] 
+   }else{
+     message=null;
+   }
+    res.render('auth/new-password', {
     path: '/new-password',
     pageTitle: 'New Password',
     errorMassage:message,
-    user:user._id.toString()
-  
+    userId:user._id.toString(),
+    passwordToken:token
+  })
   });
 };
+
+exports.postNewPassword=(req,res,next)=>{
+  const newPassword=req.body.password;
+  const userId=req.body.userId;
+  const passwordToken=req.body.passwordToken;
+   let resetUser;
+  User.findOne({resetToken:passwordToken,resetTokenExpiration:{$gt:Date.now() }
+  ,_id:userId})
+  .then(user =>{
+    resetUser=user;
+   return bcrypt.hash(newPassword,12);
+  })
+  .then(hashedPassword =>{
+   resetUser.password=hashedPassword;
+   resetUser.resetToken=null;
+   resetUser.resetTokenExpiration=undefined;
+   return resetUser.save();
+  })
+  .then(result=>{
+    res.redirect('/login')
+  })
+  .catch(err =>{
+    console.log(err);
+  });
+}
