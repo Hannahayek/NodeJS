8efaff8f74d1c8d1a263588ae183d9fddef7eff6
hayek <hayek@HANNA-PC>
2019-04-26 14:49:54 +0800

    305. Throwing Errors in Code

--- Project Mongoose/app.js
+++ Project Mongoose/app.js
@@ -62,10 +62,14 @@ app.use((req,res,next) => {
   User.findById(req.session.user._id)
 
   .then(user => {
+    if(!user){ return next();}
+
     req.user=user
    next();
   })
-  .catch(err => console.log(err));
+  .catch(err =>{
+    throw new Error(err);
+  });
 });
 
 //middle ware to user all tokens in all views
