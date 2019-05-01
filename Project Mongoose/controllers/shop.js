f971677affc19b3fb8fb3f375d54c3b341fa7df3
hayek <hayek@HANNA-PC>
2019-05-01 23:46:40 +0800

    339. Re-Using the Pagination Logic & Controls

--- Project Mongoose/controllers/shop.js
+++ Project Mongoose/controllers/shop.js
@@ -1,26 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+
+const PDFDocument = require('pdfkit');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const fs=require('fs');
-const path=require('path');
-const PDFDocument=require('pdfkit');
+const ITEMS_PER_PAGE = 2;
 
-const ITEMS_PER_PAGE=2;
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
-      console.log(products);
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
+        pageTitle: 'Products',
+        path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
-    .catch(err=>{
-      const error=new Error(err);
-      error.httpStatusCode=500;
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
       return next(error);
-     });
+    });
 };
 
 exports.getProduct = (req, res, next) => {
@@ -33,41 +50,43 @@ exports.getProduct = (req, res, next) => {
         path: '/products'
       });
     })
-    .catch(err=>{
-      const error=new Error(err);
-      error.httpStatusCode=500;
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
       return next(error);
-     });
+    });
 };
 
 exports.getIndex = (req, res, next) => {
-   const page=req.query.page;
-   let totalItems;
+  const page = +req.query.page || 1;
+  let totalItems;
 
-    Product.find().countDocuments().then(numProducts =>{
-      totalItems=numProducts;
+  Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
       return Product.find()
-      .skip((page-1)*ITEMS_PER_PAGE)
-     .limit(ITEMS_PER_PAGE)
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
     })
-     .then(products => {
+    .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-        totalProducts:totalItems,
-        hasNextPage:ITEMS_PER_PAGE *page<totalItems,
-        hasPreviousPage:package>1,
-        nextPage:page+1,
-        previousPage:page-1,
-        lastPage:Math.ceil(totalItems/ITEMS_PER_PAGE)
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
-    .catch(err=>{
-      const error=new Error(err);
-      error.httpStatusCode=500;
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
       return next(error);
-     });
+    });
 };
 
 exports.getCart = (req, res, next) => {
@@ -82,11 +101,11 @@ exports.getCart = (req, res, next) => {
         products: products
       });
     })
-    .catch(err=>{
-      const error=new Error(err);
-      error.httpStatusCode=500;
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
       return next(error);
-     });
+    });
 };
 
 exports.postCart = (req, res, next) => {
@@ -98,6 +117,11 @@ exports.postCart = (req, res, next) => {
     .then(result => {
       console.log(result);
       res.redirect('/cart');
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -108,11 +132,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .then(result => {
       res.redirect('/cart');
     })
-    .catch(err=>{
-      const error=new Error(err);
-      error.httpStatusCode=500;
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
       return next(error);
-     });
+    });
 };
 
 exports.postOrder = (req, res, next) => {
@@ -138,11 +162,11 @@ exports.postOrder = (req, res, next) => {
     .then(() => {
       res.redirect('/orders');
     })
-    .catch(err=>{
-      const error=new Error(err);
-      error.httpStatusCode=500;
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
       return next(error);
-     });
+    });
 };
 
 exports.getOrders = (req, res, next) => {
@@ -154,11 +178,11 @@ exports.getOrders = (req, res, next) => {
         orders: orders
       });
     })
-    .catch(err=>{
-      const error=new Error(err);
-      error.httpStatusCode=500;
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
       return next(error);
-     });
+    });
 };
 
 exports.getInvoice = (req, res, next) => {
@@ -175,11 +199,11 @@ exports.getInvoice = (req, res, next) => {
       const invoicePath = path.join('data', 'invoices', invoiceName);
 
       const pdfDoc = new PDFDocument();
-     /*  res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Type', 'application/pdf');
       res.setHeader(
         'Content-Disposition',
         'inline; filename="' + invoiceName + '"'
-      ); */
+      );
       pdfDoc.pipe(fs.createWriteStream(invoicePath));
       pdfDoc.pipe(res);
 
