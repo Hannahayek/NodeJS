8b5ae88a4ec7979a592d8987b96b3b91e3099282
Hanna Hayek (Pactera) <v-hahaye@microsoft.com>
2019-05-13 17:04:57 +0800

    374. Static Images & Error Handling

--- Project_RestApi/app.js
+++ Project_RestApi/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feed');
 const mongoose=require('mongoose');
+const path=require('path');
 
 const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use('/images',express.static(path.join(__dirname,'images')));
 
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
@@ -14,6 +16,13 @@ app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
     next();
 })
+
+app.use((error,req,res,next)=>{
+console.log(error)
+const status=error.statusCode || 500;
+const message=error.message;
+res.statusCode(status).json({message:message})
+});
 
 app.use('/feed', feedRoutes);
 mongoose.connect('mongodb://localhost:27017/messages').then(result =>{
--- Project_RestApi/controllers/feed.js
+++ Project_RestApi/controllers/feed.js
@@ -20,10 +20,9 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors=validationResult(req);
   if(!errors.isEmpty()){
-    return res.status(422)
-    .json({
-      message:'Validation failed, entered data not valid',
-      errors:errors.array()});
+     const error=new Error('Validation failed, entered data not valid');
+     error.statusCode=422;
+     throw error;
   }
 
   const title = req.body.title;
@@ -46,7 +45,10 @@ const post=new Post({
   });
 })
 .catch(err =>{
-  console.log(err)
+  if(!err.statusCode){
+    err.statusCode=500;
+  }
+  next(err);
 });
   
 
