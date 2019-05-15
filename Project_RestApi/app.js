9c4f1381ef330751c7a711af21bfe06a28ae2725
hayek <hayek@HANNA-PC>
2019-05-15 23:59:22 +0800

    377. Uploading Images

--- Project_RestApi/app.js
+++ Project_RestApi/app.js
@@ -3,11 +3,40 @@ const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feed');
 const mongoose=require('mongoose');
 const path=require('path');
+const multer=require('multer');
 
 const app = express();
 
+const uuidv4 = require('uuid/v4')
+ 
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function(req, file, cb) {
+        cb(null, uuidv4())
+    }
+});
+
+
+const fileFilter=(req,file,cb)=>{
+    if(
+        file.mimetype==='image/png'
+    ||file.mimetype==='image/jpg'||
+    file.mimetype==='image/jpeg'
+    )
+    {
+      cb(null,true);
+    }else{
+        cb(null,false);
+    }
+}
+
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use(
+    multer({storage:storage,fileFilter:fileFilter}).single('image')
+    )
 app.use('/images',express.static(path.join(__dirname,'images')));
 
 app.use((req,res,next)=>{
