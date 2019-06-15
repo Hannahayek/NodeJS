657fe48a9bac85fbdf2f18873b17fe432f10ba39
hayek <hayek@HANNA-PC>
2019-06-15 23:10:04 +0800

    387. Logging In & Creating JSON Web Tokens

--- Project_RestApi/routes/auth.js
+++ Project_RestApi/routes/auth.js
@@ -1,30 +1,36 @@
-const express=require('express');
-
+const express = require('express');
 const { body } = require('express-validator/check');
-const router=express.Router();
-const User=require('../models/user');
-const controller=require('../controllers/auth');
 
-router.put('/signup',[
+const User = require('../models/user');
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+
+router.put(
+  '/signup',
+  [
     body('email')
-    .isEmail()
-    .withMessage('Please use valid email')
-    .custom((value,{req})=>{
-        return User.findOne({email:value}).then(userDoc=>{
-            if(userDoc){
-                return Promise.reject('Email address already exists');
-            }
-        })
-    })
-    .normalizeEmail(),
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject('E-Mail address already exists!');
+          }
+        });
+      })
+      .normalizeEmail(),
     body('password')
-    .trim()
-    .isLength({min:5}),
+      .trim()
+      .isLength({ min: 5 }),
     body('name')
-    .trim()
-    .not().isEmpty()
+      .trim()
+      .not()
+      .isEmpty()
+  ],
+  authController.signup
+);
 
-],controller.signup);
+router.post('/login', authController.login);
 
-router.post('/login');
-module.exports=router;
+module.exports = router;
