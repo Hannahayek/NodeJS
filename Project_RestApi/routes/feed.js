15f73249e9296ef70e84d695fe12cea9cf373fc3
hayek <hayek@HANNA-PC>
2019-06-19 18:28:55 +0800

    389. Adding Auth Middleware to All Routes

--- Project_RestApi/routes/feed.js
+++ Project_RestApi/routes/feed.js
@@ -6,11 +6,14 @@ const isAuth=require('../middleware/is-auth');
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', isAuth,feedController.getPosts);
+router.get('/posts', 
+isAuth
+,feedController.getPosts);
 
 // POST /feed/post
 router.post(
   '/post',
+  isAuth,
   [
     body('title')
       .trim()
@@ -22,10 +25,13 @@ router.post(
   feedController.createPost
 );
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId',
+isAuth, 
+feedController.getPost);
 
 router.put(
-  '/post/:postId',
+  '/post/:postId'
+  ,isAuth,
   [
     body('title')
       .trim()
@@ -37,6 +43,6 @@ router.put(
   feedController.updatePost
 );
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId',isAuth, feedController.deletePost);
 
 module.exports = router;
