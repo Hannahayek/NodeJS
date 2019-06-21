1e1acdbb6fd366004fc84d190486f1167a24a9bf
hayek <hayek@HANNA-PC>
2019-06-21 15:52:43 +0800

    391. Adding Authorization Checks

--- Project_RestApi/controllers/feed.js
+++ Project_RestApi/controllers/feed.js
@@ -126,6 +126,11 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if(post.creator.toString() !== req.userId){
+          const error=new Error('Not authorized');
+          error.statusCode=403;
+          throw error;
+      }
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -154,6 +159,11 @@ exports.deletePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if(post.creator.toString() !== req.userId){
+        const error=new Error('Not authorized');
+        error.statusCode=403;
+        throw error;
+    }
       // Check logged in user
       clearImage(post.imageUrl);
       return Post.findByIdAndRemove(postId);
