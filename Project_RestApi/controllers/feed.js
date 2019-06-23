1f3ce8283c9d63cc9ee21002b8a2138183cb7c09
hayek <hayek@HANNA-PC>
2019-06-23 23:07:31 +0800

    397. Transforming "Then Catch" to "Async Await"

--- Project_RestApi/controllers/feed.js
+++ Project_RestApi/controllers/feed.js
@@ -5,33 +5,28 @@ const { validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
   const perPage = 2;
-  let totalItems;
-  Post.find()
-    .countDocuments()
-    .then(count => {
-      totalItems = count;
-      return Post.find()
+  try {
+    
+ 
+  const totalItems = await Post.find().countDocuments()
+ const posts=await Post.find()
         .skip((currentPage - 1) * perPage)
         .limit(perPage);
-    })
-    .then(posts => {
-      res
-        .status(200)
-        .json({
+   res.status(200).json({
           message: 'Fetched posts successfully.',
           posts: posts,
           totalItems: totalItems
         });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
+      } catch (err) {
+        if (!err.statusCode) {
+          err.statusCode = 500;
+        }
+        next(err);
       }
-      next(err);
-    });
+    
 };
 
 exports.createPost = (req, res, next) => {
