9ce17b16574f39e3a440cb3b150a3af5b4442f9a
hayek <hayek@HANNA-PC>
2019-07-02 15:58:41 +0800

    423. Adding a Login Query & a Resolver

--- Project_RestApi_Graph/graphQl/resolvers.js
+++ Project_RestApi_Graph/graphQl/resolvers.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt=require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -20,5 +21,28 @@ module.exports = {
     });
     const createdUser = await user.save();
     return { ...createdUser._doc, _id: createdUser._id.toString() };
+  },
+  login:async function({email,password}){
+    const user=await User.findOne({email:email});
+    if(!user){
+      const error =new Error("user not found");
+      error.code=401;
+      throw error;
+    }
+    const isEqual=await bcrypt.compare(password,user.password);
+
+    if(!isEqual){
+      const error=new Error("wrong password");
+      error.code=201;
+      throw error;
+    }
+    const token=jwn.sign(
+      {
+      userId:user._id.toString(),
+      email:user.email
+    },'someSecretweusefor the token',
+    {expiresIn:'1h'}
+    );
+    return {token: token,userId :user._id.toString()};
   }
 };
